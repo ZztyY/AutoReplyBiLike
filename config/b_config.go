c4adee5cb9d8c83ee3eda6ec126e5afabf6e7596
zhengtianyi <zhengtianyi@baixing.com>
2020-12-07 10:42:24 +0800

    私信修复

--- config/b_config.go
+++ config/b_config.go
@@ -1,7 +1,7 @@
 package config
 
 // urls
-const BILIBILI_MSGFEED_LIKE = "https://api.bilibili.com/x/msgfeed/like"              // 点赞内容与用户概览
-const BILIBILI_LIKE_DETAIL = "https://api.bilibili.com/x/msgfeed/like_detail"        // 点赞用户详情
-const BILIBILI_SEND_MESSAGE = "http://api.vc.bilibili.com/web_im/v1/web_im/send_msg" // 发送私信
-const BILIBILI_NAV = "http://api.bilibili.com/nav"                                   // 导航栏详情
+const BILIBILI_MSGFEED_LIKE = "https://api.bilibili.com/x/msgfeed/like"               // 点赞内容与用户概览
+const BILIBILI_LIKE_DETAIL = "https://api.bilibili.com/x/msgfeed/like_detail"         // 点赞用户详情
+const BILIBILI_SEND_MESSAGE = "https://api.vc.bilibili.com/web_im/v1/web_im/send_msg" // 发送私信
+const BILIBILI_NAV = "https://api.bilibili.com/nav"                                   // 导航栏详情
--- main.go
+++ main.go
@@ -3,13 +3,13 @@ package main
 import (
 	"AutoReplyBiLike/api"
 	"AutoReplyBiLike/config"
-	"fmt"
 )
 
 func main() {
-	likeCardIdList := api.GetLikeCards(config.SESSDATA)
-	for _, v := range likeCardIdList {
-		likeList := api.GetLikeUserIds(v, config.SESSDATA)
-		fmt.Print(likeList)
-	}
+	//likeCardIdList := api.GetLikeCards(config.SESSDATA)
+	//for _, v := range likeCardIdList {
+	//	likeList := api.GetLikeUserIds(v, config.SESSDATA)
+	//	fmt.Print(likeList)
+	//}
+	api.SendMessage(320170411, 216174037, config.SESSDATA, "test")
 }
